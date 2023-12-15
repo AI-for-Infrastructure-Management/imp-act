c398a46c73783648ff4c332fc18d66127a075d55
prateek <pbhustali@tudelft.nl>
2023-12-15 11:48:18 +0100

    Changed logic of episode rollout
    Earlier: Counting time-steps
    New: Checking done flag

--- test_environment.py
+++ test_environment.py
@@ -2,14 +2,16 @@ import pytest
 
 from environment import RoadEnvironment
 from environment_presets import *
+import igraph as ig
 
 @pytest.fixture
 def small_environment():
+    """Create a small environment for testing."""
     env = RoadEnvironment(**small_environment_dict)
     return env
 
 def test_observation_keys(small_environment):
-    """Test if the observation dictionary has the correct keys. In reset and step functions."""
+    """Test if the observation dictionary has the correct keys in reset and step functions."""
 
     env = small_environment
     obs = env.reset()
@@ -31,16 +33,14 @@ def test_observation_keys(small_environment):
 def test_one_episode(small_environment):
     """Test if the environment can run one episode."""
     env = small_environment
-    
-    obs = env.reset()
-    actions = [[1,1] for _ in range(4)]
-    timestep = 0
 
-    max_timesteps = 1000
-    while timestep < max_timesteps:
+    _ = env.reset()
+    actions = [[1,1] for _ in range(len(env.edge_segments_numbers))]
+    timestep = 0
+    done = False
+
+    while not done:
         timestep += 1
         obs, cost, done, info = env.step(actions)
-        if done:
-            break
 
-    assert timestep == small_environment_dict["max_timesteps"]
+    assert timestep == env.max_timesteps
