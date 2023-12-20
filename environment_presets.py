8ea4849aa1a438fa0a08be4a6af78f527dbd827d
Prateek Bhustali <61668356+omniscientoctopus@users.noreply.github.com>
2023-12-20 17:47:30 +0100

    Added env preset with 3 vertices
    To circumvent the curse of dimensionality, I added a small env preset.

--- environment_presets.py
+++ environment_presets.py
@@ -5,3 +5,11 @@ small_environment_dict = {
     "trips": [(0, 1, 200), (1, 3, 200), (2, 0, 200), (3, 2, 200)],
     "max_timesteps": 50,
 }
+
+smallest_environment_dict = {
+    "num_vertices": 3,
+    "edges": [(0, 1), (0, 2)],
+    "edge_segments_numbers": [1, 1],
+    "trips": [(0, 1, 200), (0, 2, 200)],
+    "max_timesteps": 20,
+}
