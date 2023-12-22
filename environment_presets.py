fe8a20972adc4336c0237b662cead3fc6ea3686c
Daniel Hettegger <daniel.hettegger@tum.de>
2023-12-22 17:30:00 +0100

    Move fixed seed to test file to prevent seeding env everytime preset is loaded.

--- environment_presets.py
+++ environment_presets.py
@@ -4,5 +4,4 @@ small_environment_dict = {
     "edge_segments_numbers": [2, 2, 2, 2],
     "trips": [(0, 1, 200), (1, 3, 200), (2, 0, 200), (3, 2, 200)],
     "max_timesteps": 50,
-    "seed": 42
 }
