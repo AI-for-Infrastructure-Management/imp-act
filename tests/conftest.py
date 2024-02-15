40f365f3c036769f3313a7dfc75d52d2317d1e74
Daniel Hettegger <daniel.hettegger@tum.de>
2024-02-15 10:15:50 +0100

    small environment is now Montenegro

--- tests/conftest.py
+++ tests/conftest.py
@@ -3,22 +3,34 @@ from environments.config.environment_loader import EnvironmentLoader
 
 
 @pytest.fixture
+def toy_environment_path():
+    """Path to toy environment file"""
+    return "environments/config/environment_presets/toy_environment.yaml"
+
+
+@pytest.fixture
+def toy_environment_loader(toy_environment_path):
+    """Create a toy environment loader for testing."""
+    return EnvironmentLoader(toy_environment_path)
+
+
+@pytest.fixture
+def toy_environment(toy_environment_loader):
+    """Create a toy environment loader for testing."""
+    env = toy_environment_loader.to_numpy()
+    return env
+
+
+@pytest.fixture
 def small_environment_path():
     """Path to small environment file"""
     return "environments/config/environment_presets/small_environment.yaml"
 
 
 @pytest.fixture
-def small_environment_loader(small_environment_path):
-    """Create a small environment loader for testing."""
-    return EnvironmentLoader(small_environment_path)
-
-
-@pytest.fixture
-def small_environment(small_environment_loader):
-    """Create a small environment loader for testing."""
-    env = small_environment_loader.to_numpy()
-    return env
+def small_environment(small_environment_path):
+    loader = EnvironmentLoader(small_environment_path)
+    return loader.to_numpy()
 
 
 @pytest.fixture
