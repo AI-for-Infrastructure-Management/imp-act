4653d2449bb99bcef6d5add9f5b6e37396d09277
giarcieri <arcieri.1587763@studenti.uniroma1.it>
2024-02-29 12:44:07 +0100

    adapted code to env loader

--- profiling/jax_vs_numpy.py
+++ profiling/jax_vs_numpy.py
@@ -3,25 +3,30 @@ import time
 import jax
 import jax.numpy as jnp
 
-from environment import RoadEnvironment as NumPyRoadEnvironment
-from environment_presets import small_environment_dict
-
-from environments.jax_environment import JaxRoadEnvironment
-from params import EnvParams
+from environments.config.environment_loader import EnvironmentLoader
 
 
-def do_nothing_policy(env, obs):
-    return [[0] * segments for segments in env.edge_segments_numbers]
+def do_nothing_policy_np(edge_segments_numbers):
+    return [[0] * segments for segments in edge_segments_numbers]
 
 
-def numpy_rollout(env, policy):
+def do_nothing_policy_jax(edge_segments_numbers):
+    action_dict = {}
+    for i, n_segments in enumerate(edge_segments_numbers):
+        action_dict[f"{i}"] = [0] * n_segments
+    _action = [action_dict]
+    __action = jax.tree_util.tree_leaves(_action)
+    action = jnp.array(__action, dtype=jnp.uint8)
+    return action
+
+
+def numpy_rollout(env, edge_segments_numbers, actions):
     obs = env.reset()
     done = False
 
     total_reward = 0
 
     while not done:
-        actions = policy(env, obs["edge_observations"])
         next_obs, reward, done, _ = env.step(actions)
 
         obs = next_obs
@@ -30,13 +35,32 @@ def numpy_rollout(env, policy):
     return total_reward
 
 
-if __name__ == "__main__":
+def compute_edge_segments_numbers_np(env):
+    edge_segments_numbers_np = []
+    for edge in env.graph.es:
+        edge_segments_numbers_np.append(edge["road_segments"].number_of_segments)
+    return edge_segments_numbers_np
+
+
+def compute_edge_segments_numbers_jax(env):
+    edge_segments_numbers_jax = []
+    for edge in env.segments_list:
+        edge_segments_numbers_jax.append(len(edge))
+    return edge_segments_numbers_jax
+
+
+def main(filename):
     experiments = [1, 10, 100, 1_000, 10_000]
 
     store_returns_for = experiments[-1]
 
+    env_loader = EnvironmentLoader(filename)
+
     # NUMPY
-    env = NumPyRoadEnvironment(**small_environment_dict)
+    env = env_loader.to_numpy()
+    edge_segments_numbers_np = compute_edge_segments_numbers_np(env)
+
+    actions_np = do_nothing_policy_np(edge_segments_numbers_np)
 
     numpy_timings = []
     numpy_returns = []
@@ -45,7 +69,7 @@ if __name__ == "__main__":
         start = time.time()
 
         for _ in range(NUM_EPISODES):
-            total_reward = numpy_rollout(env, do_nothing_policy)
+            total_reward = numpy_rollout(env, edge_segments_numbers_np, actions_np)
 
             if NUM_EPISODES == store_returns_for:
                 numpy_returns.append(total_reward)
@@ -55,21 +79,19 @@ if __name__ == "__main__":
         numpy_timings.append(end - start)
 
     # JAX
-    params = EnvParams()
-    jax_env = JaxRoadEnvironment(params)
+    jax_env = env_loader.to_jax()
+    edge_segments_numbers_jax = compute_edge_segments_numbers_jax(jax_env)
 
-    _action = [{"0": [0, 0]}, {"1": [0, 0]}, {"2": [0, 0]}, {"3": [0, 0]}]
-    __action = jax.tree_util.tree_leaves(_action)
-    action = jnp.array(__action, dtype=jnp.uint8)
-
+    action_jax = do_nothing_policy_jax(edge_segments_numbers_jax)
     jax_timings = []
     jax_returns = []
+
+    key = jax.random.PRNGKey(442)
 
     for NUM_EPISODES in experiments:
 
         start_jax = time.time()
 
-        key = jax.random.PRNGKey(442)
         step_keys, key = jax_env.split_key(key)
 
         # rollout
@@ -83,7 +105,7 @@ if __name__ == "__main__":
             for _ in range(jax_env.max_timesteps):
                 # step
                 obs, reward, done, info, state = jax_env.step_env(
-                    step_keys, state, action
+                    step_keys, state, action_jax
                 )
 
                 step_keys, key = jax_env.split_key(key)
@@ -99,7 +121,7 @@ if __name__ == "__main__":
 
     # Print results
     print(f"NumPy timings: {numpy_timings}")
-    print(f"Jax static timings: {jax_timings}")
+    print(f"Jax timings: {jax_timings}")
 
     # Plot results
     import matplotlib.pyplot as plt
@@ -123,3 +145,14 @@ if __name__ == "__main__":
     ax[1].legend()
 
     plt.show()
+
+
+if __name__ == "__main__":
+    paths = [
+        "environments/config/environment_presets/toy_environment.yaml",
+        # "environments/config/environment_presets/small_environment.yaml",
+        # "environments/config/environment_presets/large_environment.yaml"
+    ]
+    for filename in paths:
+        print(filename)
+        main(filename)
