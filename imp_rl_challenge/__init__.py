4b95e77e0f0fd5f12a9223beb6f4446245fd7cdc
Daniel Hettegger <daniel.hettegger@tum.de>
2024-03-04 11:51:55 +0100

    Move all files into package folder

---
+++ imp_rl_challenge/__init__.py
@@ -0,0 +1,6 @@
+from .environments.registry import Registry
+
+def get_environment(environment_name):
+    """Given a registered environment name return a fully initialized environment instance."""
+    registry = Registry()
+    return registry.make(environment_name)
