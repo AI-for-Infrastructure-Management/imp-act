42c8a013f9cd419ee02bfdf03e26b13403c9dbb3
Prateek Bhustali <61668356+omniscientoctopus@users.noreply.github.com>
2024-10-30 23:40:07 +0100

    apply linter

--- imp_act/environments/rollout_plotter.py
+++ imp_act/environments/rollout_plotter.py
@@ -1,9 +1,10 @@
 from itertools import chain
 
+import matplotlib.patches as patches
+
 import matplotlib.pyplot as plt
 import numpy as np
 from matplotlib.lines import Line2D
-import matplotlib.patches as patches
 
 from imp_act.environments.recorder import Recorder
 
